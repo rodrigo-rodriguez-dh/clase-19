d92faa13c337673dc9df2d06a740a90b068688a7
Rodrigo Rodriguez <110953695+rodrigo-rodriguez-dh@users.noreply.github.com>
2022-09-13 12:30:46 -0300

    Add files via upload

---
+++ make.sh
@@ -0,0 +1,28 @@
+cd eureka-server/
+mvn clean && mvn package -DskipTests
+docker build . -t eureka-server
+cd ..
+
+cd server-config/
+sh make.sh
+cd ..
+
+cd movie-service/
+mvn clean && mvn package -DskipTests
+docker build . -t movie-service
+cd ..
+
+cd catalog-service/
+mvn clean && mvn package -DskipTests
+docker build . -t catalog-service
+cd ..
+
+cd api-gateway/
+sh make.sh
+cd ..
+
+cd series/
+sh make.sh
+cd ..
+
+docker-compose up
